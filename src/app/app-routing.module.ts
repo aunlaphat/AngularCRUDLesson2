45b6a014373ff4d745e9b99fb066d76a9bee9161
aunlaphat <aunlaphat5@hotmail.com>
2024-04-04 17:16:48 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HotelDetailComponent } from './components/hotel-detail/hotel-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'hotels', component: HotelsComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HotelDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
